95754e4be31185d5ac3da8dc583aa5259fe7f17e
Phil Roan <4581471+proan@users.noreply.github.com>
2024-11-22 07:48:15 -0800

    Fix indent

--- ecl_io/include/ecl/io/sockets.hpp
+++ ecl_io/include/ecl/io/sockets.hpp
@@ -76,7 +76,7 @@ public:
   SocketError(const ErrorFlag& flag = UnknownError) : Error(flag)
   {}
   using Error::operator=;
- protected:
+protected:
   virtual const char* invalidArgErrorString() const
   { return "One of the arguments is invalid (usually a socket descriptor).";}
 #ifdef ECL_IS_WIN32
